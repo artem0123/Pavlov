18d20099f762b74577dbf18dc29ed9cc960d90a8
Artem <pavlov.artem02@mail.ru>
2020-12-26 15:38:56 +0700

    Initial commit hello

---
+++ Pavlovv8/app/src/main/java/com/example/pavlov_v_8/TwoScreen.java
@@ -0,0 +1,14 @@
+package com.example.pavlov_v_8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwoScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_screen);
+    }
+}
